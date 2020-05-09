7e05751da992bbb8893b8286834434c4d629af38
javellej <jerome.javelle@gmail.com>
2020-05-09 15:00:19 +0000

    add compile-time range check to QReg

--- main/qreg.hpp
+++ main/qreg.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <types.hpp>
+#include <constants.hpp>
 #include <limits>
+#include <cassert>
 
 /**
  * Simple abstract wrapper around an integer index used for type safety.
@@ -14,7 +16,7 @@ public:
     QReg() : m_index(std::numeric_limits<typeof(m_index)>::max()) {}
 
     constexpr
-    QReg( u32 reg_index ) : m_index(reg_index) {}
+    QReg( u32 reg_index ) : m_index(reg_index) { assert( k_num_qubits > reg_index ); }
 
 private:
     u32 m_index;
